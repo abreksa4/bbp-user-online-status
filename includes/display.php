82b5fe31a472d23181c795d05e094a912595bfb4
Andrew Breksa <abreksa4@users.noreply.github.com>
2015-08-16 11:29:14 -0700

    Added CSS styling
    copied code from jacobwarduk/bbp-user-online-status

--- includes/display.php
+++ includes/display.php
@@ -24,14 +24,20 @@ function bbp_user_online_status(){
     echo '<ul>';
     $user_id = bbp_get_reply_author_id($reply_id);
     if ($bbpuos_options['activate'] == true) {
-        echo '<li>';
-        if (is_user_online($user_id)) {
-            echo "Online";
-        } else {
-            echo "Offline";
-        }
-        echo '</li>';
-    }
+         echo '<ul>';
+     $user_id = bbp_get_reply_author_id($reply_id);
+     if ($bbpuos_options['activate'] == true) {
+-        echo '<li>';
++        echo '<li class="bbp-online-status">';
+         if (is_user_online($user_id)) {
+-            echo "Online";
++            echo '<span class="bbp-online">Online</span>';
+         } else {
+-            echo "Offline";
++            echo '<span class="bbp-offline">Offline</span>';
+         }
+         echo '</li>';
+     }
 }
 
 add_action('wp_logout', 'set_user_logged_out');
